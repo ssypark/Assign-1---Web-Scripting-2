8b283a5da07ca0c238ef27829f282b9291b6c158
hobbybill <sungyool.park@gmail.com>
2024-10-29 18:11:50 -0700

    update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    <h1 className="text-2xl font-bold mb-4">Hello World</h1>
+    </>
+  )
+}
+
+export default App
